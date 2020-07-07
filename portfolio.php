32b05862b380146656536d474533a9e9f4d492bc
Alexa Vermenot <alexa.vermenot@orange.fr>
2020-07-07 16:58:09 +0200

    page2 et 3

--- portfolio.php
+++ portfolio.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Alexa Vermenot">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Portefolio</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -12,47 +11,68 @@
 
   <body data-spy="scroll" data-target="#myScrollspy" data-offset="1">
 
-  <div class="container-fluid">
-      <nav class="col-sm-6 col-8" id="myScrollspy">
-        <!-- <ul class="nav nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" href="#section1">Acceuil</a>
-          </li> -->
-          <li class="nav-item d">
-            <a class="nav-link" href="#section1">A propos</a>
-          </li>
-          <li class="nav-item g">
-            <a class="nav-link" href="#section2">Projets</a>
-          </li>
-          <li class="nav-item d">
-            <a class="nav-link" href="#section3">Articles</a>
-          </li>
-          <li class="nav-item g">
-            <a class="nav-link" href="#section4">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <div class="container-fluid">
+      <div id="top"><a class="nav-link" href="#home">
+        <img src="img/treesf.png" alt="accueil"></a>
+        <span>Portfolio Alexa Vermenot</span>
 
-      <div class="col-sm-4 col-6">
-        <section id="section1">
-          <h1>Section 1</h1>
-          <p>Try to scroll this page and look at the menu while scrolling!</p>
-        </section>
-        <section id="section2">
-          <h1>Section 2</h1>
-          <p>Try to scroll this page and look at the menu while scrolling!</p>
-        </section>
-        <section id="section3">
-          <h1>Section 3</h1>
-          <p>Try to scroll this page and look at the menu while scrolling!</p>
-        </section>
-        <section id="section4">
-          <h1>Section 4</h1>
-          <p>Try to scroll this page and look at the menu while scrolling!</p>
-        </section>
       </div>
 
+      <div class="container" id="nav">
+        <nav class="col-sm-6 col-8" id="myScrollspy">
+           <ul class="nav nav flex-column">
+            <li class="nav-item d col-sm3 col-4 ">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item g col-sm3 col-4">
+              <a class="nav-link" href="#projets">Projets</a>
+            </li>
+            <li class="nav-item d col-sm3 col-4">
+              <a class="nav-link" href="#article">Articles</a>
+            </li>
+            <li class="nav-item g col-sm3 col-4">
+              <a class="nav-link" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+
+    <div class="container-fluid">
+      <section class="row" id="about">
+        <h1 class="col-sm-12 col-lg-6">About</h1>
+        <p class="col-sm-12 col-lg-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Try to scroll this page and look at the menu while scrolling!</p>
+      </section>
+    </div>
+    <div class="container-fluid">
+      <section class="row" id="projets">
+        <h1 class="col-sm-12 col-lg-6">Projets</h1>
+        <div class="card-body col-sm-12 col-lg-6">
+          <div class="imageprojet">
+            <a href="#pageprojet" class="card-link"><img src="img/Screenshot-projet-integration-bootstrap-.png" alt="bslite"></a>
+          </div>
+          <div class="imageprojet">
+            <a href="#pageprojet" class="card-link"><img src="img/Screenshot-projet-integration-html-css-.png" alt="gratia"></a>
+          </div>
+          <div class="imageprojet">
+            <a href="#pageprojet" class="card-link"><img src="img/bomberbird.png" alt="bomberbird"></a>
+          </div>
+        </div>
+      </section>
+    </div>
+    <div class="container-fluid">
+      <section class="row" id="section3">
+        <h1>Section 3</h1>
+        <p>Try to scroll this page and look at the menu while scrolling!</p>
+      </section>
+    </div>
+    <div class="container-fluid">
+      <section class="row" id="section4">
+        <h1>Section 4</h1>
+        <p>Try to scroll this page and look at the menu while scrolling!</p>
+      </section>
+    </div>
 
 
 </body>
